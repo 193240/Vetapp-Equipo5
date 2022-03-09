230321d73a51401cb98eb7e747456037c8f7cce2
SR <193240@ids.upchiapas.edu.mx>
2022-03-08 20:13:14 -0600

    auth

--- vista-veterinaria/src/index.js
+++ vista-veterinaria/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Componentes/assets/css/registro.css'
 ReactDOM.render(
   <BrowserRouter>
     <App />
